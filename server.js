179754df828d4a995873818a4890fc784a3350a1
fvacreate <78551936+vladislavfva@users.noreply.github.com>
2023-05-12 13:56:15 +0200

    a

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 443;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/form-data', (req, res) => {
+  const formData = req.body;
+  console.log(formData); // Вывод полученных данных в консоль
+
+  // Ваш код обработки данных формы здесь
+
+  res.send('Данные успешно отправлены');
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
