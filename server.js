209b2ce6a8f5b47078aaa24b5815db30fc6d2fd7
fvacreate <78551936+vladislavfva@users.noreply.github.com>
2023-05-12 14:04:15 +0200

    s

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const port = 443;
+const port = process.env.PORT || 443;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.post('/form-data', (req, res) => {
+app.post('/form-submit', (req, res) => {
   const formData = req.body;
-  console.log(formData); // Вывод полученных данных в консоль
-
-  // Ваш код обработки данных формы здесь
-
-  res.send('Данные успешно отправлены');
+  console.log(formData);
+  // здесь можно добавить код для обработки данных формы
+  res.send('Данные формы успешно отправлены на сервер');
 });
 
 app.listen(port, () => {
