c10479d16ec74be16cf935c7a22153a5a429744e
fvacreate <78551936+vladislavfva@users.noreply.github.com>
2023-05-17 22:01:23 +0200

    s3

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
 const port = process.env.PORT || 443;
@@ -12,10 +14,15 @@ app.use(cors());
 app.post('/form-submit', (req, res) => {
   const formData = req.body;
   console.log(formData);
-  // здесь можно добавить код для обработки данных формы
+  // Здесь можно добавить код для обработки данных формы
   res.send('Данные формы успешно отправлены на сервер');
 });
 
-app.listen(port, () => {
+const options = {
+  key: fs.readFileSync('/path/to/private.key'),
+  cert: fs.readFileSync('/path/to/certificate.crt'),
+};
+
+https.createServer(options, app).listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
